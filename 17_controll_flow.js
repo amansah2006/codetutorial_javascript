2c595a0ab2d56434bd8bf40a83e06048c1f4bfd0
amansah2006 <aman732006@gmail.com>
2026-07-19 13:04:04 +0530

    Add files via upload

---
+++ 17_controll_flow.js
@@ -0,0 +1,23 @@
+// if 
+/*  
+if(true){ //agr condition  true hua to ander ka scope run hoga  
+
+}
+if(false){ //agr condition false hua to scope ke ander ka code run nahi hoga
+
+} */
+
+// comparision operator
+/* 
+<,>,<=,>=,==,!=,===(ye datatype ko bhi compare krlega),!==
+
+*/
+// for example
+
+const roll_no = 45
+if(roll_no>40){
+    console.log("pass out");
+}else{
+    console.log("fail ");
+    
+}
